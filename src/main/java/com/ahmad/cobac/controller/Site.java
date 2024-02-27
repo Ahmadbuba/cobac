367d94c1fc218fa0b4f4530882710a7298385cd5
Ahmadbuba <ahmadbubacherryfield@gmail.com>
2024-02-27 19:35:47 +0100

    bug: @gettMapping not added to the index controller

--- src/main/java/com/ahmad/cobac/controller/Site.java
+++ src/main/java/com/ahmad/cobac/controller/Site.java
@@ -1,9 +1,11 @@
 package com.ahmad.cobac.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Site {
+    @GetMapping("/")
     public String home() {
         return "index";
     }
