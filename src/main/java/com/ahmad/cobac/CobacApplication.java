a709033407b13ad4b351382d8886a557ab99d661
Ahmadbuba <ahmadbubacherryfield@gmail.com>
2024-02-27 19:30:21 +0100

    initial commit

---
+++ src/main/java/com/ahmad/cobac/CobacApplication.java
@@ -0,0 +1,13 @@
+package com.ahmad.cobac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmad/cobac/CobacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmad.cobac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
